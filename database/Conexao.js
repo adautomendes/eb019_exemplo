167e8e987a7ea908043c4c995746cad3f3ea4df2
Adauto Mendes Bernardes Júnior <adauto.mendes@inatel.br>
2020-02-18 07:46:32 -0300

    Renomeando arquivos

--- database/Conexao.js
+++ database/Conexao.js
@@ -2,13 +2,13 @@
 const { Sequelize } = require("sequelize");
 
 /**
- * eb019_2020_1_exemplo => nome do BD
+ * eb019_exemplo => nome do BD
  * root => usuário do BD
  * '' => senha do BD
  * host: 'localhost' => onde o BD está rodando, mantenha 'localhost' se estiver rodando na sua máquina
  * dialect: 'mysql' => dialeto com o qual você quer se comunicar com o BD, mantenha 'mysql' se estiver usando MySQL
  */
-const sequelize = new Sequelize('eb019_2020_1_exemplo', 'root', '', {
+const sequelize = new Sequelize('eb019_exemplo', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
